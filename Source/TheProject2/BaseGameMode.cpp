bb454be9734f94050423380a212f45ac12411a61
BearSur <liu20041218@outlook.com>
2026-02-12 21:45:41 +0800

    更新文件结构

---
+++ Source/TheProject2/BaseGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BaseGameMode.h"
+
+ABaseGameMode::ABaseGameMode()
+{
+	// stub
+}
--- Source/TheProject2/TheProject2GameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TheProject2GameMode.h"
-
-ATheProject2GameMode::ATheProject2GameMode()
-{
-	// stub
-}
